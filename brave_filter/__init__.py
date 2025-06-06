7a2f4aee4461a9f35f456babb557b6a94b5466dc
Murtaza Salih <114719466+itsrody@users.noreply.github.com>
2025-06-06 20:03:32 +0300

    Delete brave_filter directory

--- brave_filter/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Unified filter list generator for Brave."""
-
-from .fetcher import fetch_filters
-from .generator import generate_unified_list
-
-__all__ = ["fetch_filters", "generate_unified_list"]
